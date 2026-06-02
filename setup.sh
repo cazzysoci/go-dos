8aab75935bc1a38cbeed4e8375142d953f34fb5f
cazzysoci <carielglobio@gmail.com>
2026-06-02 12:40:58 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -750,7 +750,7 @@ var (
 "https://mihanblog.com/",
 "https://interia.pl/",
 "https://goo.gl/",
-"https://ensonhaber.com/"
+"https://ensonhaber.com/",
 "https://www.google.com/",
 "https://www.bing.com/",
 "https://duckduckgo.com/",
@@ -1644,21 +1644,6 @@ func attackWorker(target, mode string, done chan struct{}, stats *atomicCounter,
 }
 EOF
 
-sed -i '/acceptLanguages = \[\]string{/,/}/c\
-acceptLanguages = []string{\
-	"en-US,en;q=0.9",\
-	"en-US,en;q=0.9,fr;q=0.8,de;q=0.7,es;q=0.6,ja;q=0.5,zh-CN;q=0.4",\
-	"en-GB,en;q=0.9,en-US;q=0.8",\
-	"en-US,en;q=0.9,es;q=0.8,pt;q=0.7",\
-	"en-US,en;q=0.9,ru;q=0.8,uk;q=0.7",\
-	"en-US,en;q=0.9",\
-	"fr-FR,fr;q=0.9",\
-	"de-DE,de;q=0.9",\
-	"es-ES,es;q=0.9",\
-	"ja-JP,ja;q=0.9",\
-	"zh-CN,zh;q=0.9",\
-	"ru-RU,ru;q=0.9",\
-}' main.go
 
 if [ ! -f "main.go" ]; then
     echo -e " ${RED}✗ Error: Failed to create main.go${NC}"
